3c489c4ad16e3a9393415a54d10d6196fe83c4f5
Ali Ahmed <alyahmedrbk@gmail.com>
2025-03-20 21:44:20 +0500

    dashboard created and deisgned and functions added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,10 @@ import Applications from './pages/Applications';
 import RecuruterLogin from './components/RecuruterLogin';
 import { useContext } from 'react';
 import { AppContext } from './context/AppContext';
+import Dashboard from './pages/Dashboard';
+import AddJob from './pages/AddJob';
+import ManageJobs from './pages/ManageJobs';
+import ViewApplication from './pages/ViewApplication';
 
 
 const App = () => {
@@ -19,6 +23,11 @@ const App = () => {
             <Route path='/' element={<Home />} />
             <Route path='/apply-job/:id' element={<ApplyJob />} />
             <Route path='/application' element={<Applications />} />
+            <Route path='/dashboard' element={<Dashboard />}>
+              <Route path='add-job' element={<AddJob />} />
+              <Route path='manage-job' element={<ManageJobs />} />
+              <Route path='view-application' element={<ViewApplication />} />
+            </Route> 
         </Routes>
     </div>
   )
---
+++ frontend/src/pages/ViewApplication.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ViewApplication = () => {
+  return (
+    <div>
+        View
+    </div>
+  )
+}
+
+export default ViewApplication
