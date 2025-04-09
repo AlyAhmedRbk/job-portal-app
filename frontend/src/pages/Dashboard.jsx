3f23be50209ad843fcfcc9f842806bbc0a3f4709
Ali Ahmed <alyahmedrbk@gmail.com>
2025-04-09 18:16:35 +0500

    The ui design completed

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -14,7 +14,7 @@ const Dashboard = () => {
             <div className='px-5 flex justify-between items-center'>
                 <img onClick={() => navigate('/')} className='max-sm:w-32 cursor-pointer' src={assets.logo} alt="" />
                 <div className='flex items-center gap-3'>
-                    <p className='max-sm:hidden'>Welcome, Great Stack</p>
+                    <p className='max-sm:hidden'>Welcome, Ali Ahmed</p>
 
                     <div className='relative group'>
                         <img className='w-8 border rounded-full' src={assets.company_icon} alt="" />
