76480e4503f675f9395decd1461e3432ebb0d48b
Ali Ahmed <alyahmedrbk@gmail.com>
2025-04-10 17:47:59 +0500

    installing and configuring sentry

---
+++ backend/config/instrument.js
@@ -0,0 +1,6 @@
+import * as Sentry from "@sentry/node";
+
+
+Sentry.init({
+  dsn: "https://15dc60a2a006dc85f1482e9b535a8fb8@o4509128972828672.ingest.us.sentry.io/4509128980561920",
+});
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './config/db.js';
-
+import './config/instrument.js';
+import * as Sentry from "@sentry/node"
 
 // Initializing Express
 const app = express();
@@ -14,10 +15,17 @@ app.use(express.json());
 // DB Connection
 await connectDB();
 
+// Senetry Config
+Sentry.setupExpressErrorHandler(app);
+
 // Routes
 app.get("/", (req, res) => {
     res.send('API Working...')
 });
+
+app.get("/debug-sentry", function mainHandler(req, res) {
+    throw new Error("My first Sentry error!");
+  });
 
 const PORT = process.env.PORT || 5000;
 
